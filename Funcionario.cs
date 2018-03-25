8860d68e380b3646774d171b86c4f56e246fcfc2
LucasRoKK <37778908+LucasRoKK@users.noreply.github.com>
2018-03-25 15:02:15 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salarioBruto;
+        public double desconto;
+
+        public double SalarioLiquido()
+        {
+            return salarioBruto - desconto;
+        }
+
+        public void AumentarSalario(double porc)
+        {
+            double val;
+            val = salarioBruto * (porc/100);
+            salarioBruto = salarioBruto + val;
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine("Dados do funcionário: {0}, {1}", this.nome, this.SalarioLiquido());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Empresa Zé dos Noias-----");
+            Console.WriteLine();
+
+            double porc;
+            Funcionario func = new Funcionario();
+            Console.Write("Nome: ");
+            func.nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            func.salarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Desconto: ");
+            func.desconto = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            func.Imprime();
+
+            Console.Write("Deseja aumetar o salário em qual porcentagem? ");
+            porc = double.Parse(Console.ReadLine());
+            func.AumentarSalario(porc);
+
+            func.Imprime();
+        }
+    }
+}
